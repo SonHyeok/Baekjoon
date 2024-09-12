e63d431e32c02cb72a96ae0832078def7f68e7ea
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-09-12 23:01:12 +0900

    진법 변환2 주석 추가

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -23,14 +23,14 @@ public class Main {
             int remain = num % formation;
 
             if (remain >= 10 && remain <= 35) {
-                char word = (char) ('A' + remain - 10);
-                sb.append(word);
+                char word = (char) ('A' + remain - 10); // 10이상의 수는 문자로 처리
+                sb.insert(0, word); // 스택처럼 선입선출이여야 하기에 뒤의 계산 결과를 앞으로 넣음
             }else{
-                sb.append(remain);
+                sb.insert(0,remain); // 스택처럼 선입선출이여야 하기에 뒤의 계산 결과를 앞으로 넣음
             }
             num = num / formation;
         }
-        System.out.println(sb.reverse());
+        System.out.println(sb));
     }
 }
 
