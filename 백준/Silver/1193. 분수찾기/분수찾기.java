aa23feff6fea2e73e76e766a3fe052d07eec8fbf
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-09-18 17:20:47 +0900

    [Silver V] Title: 분수찾기, Time: 72 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    /**
+     * 9 입력
+     * total = 10, level = 4, findNum = 9
+     *
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int findNum = Integer.parseInt(br.readLine());
+
+        int total = 0;
+        int level = 1; // 증가하는 칸의 개수
+
+        while (true) { // 칸의 개수만큼 더하다가 목표 칸수보다 커지면 멈춤
+            total += level;
+
+            if(total >= findNum){
+
+                // 대각 칸의 개수 짝수 기준
+                int top = findNum - (total - level); // 분자
+                int bottom = (level + 1) - top; // 분모
+                
+                if (level % 2 == 0) { // 대각 칸 개수 짝수인 경우
+                    System.out.println(top + "/" + bottom);
+                    break;
+                }else{ // 대각 칸 개수 홀수인 경우
+                    System.out.println(bottom + "/" + top);
+                    break;
+                }
+            }else{
+                level++;
+            }
+        }
+    }
+}
