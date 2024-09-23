9ed984ec448052e482a6edb07958de87f5d2971c
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-09-23 23:20:31 +0900

    [Silver V] Title: 배열 합치기, Time: 1152 ms, Memory: 327776 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,36 +1,55 @@
 import java.io.*;
 import java.util.StringTokenizer;
-
-import static java.util.Arrays.sort;
-
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int list1_length = Integer.parseInt(st.nextToken()); // 첫번째 배열의 길이
-        int list2_length = Integer.parseInt(st.nextToken()); // 두번째 배열의 길이
-        int[] total_list = new int[list1_length + list2_length]; // 두 배열의 길이를 합친 최종 배열
 
-        st = new StringTokenizer(br.readLine()); // 첫번째 배열에 저장할 정수 입력
-        for (int i = 0; i < total_list.length; i++) {
-            total_list[i] = Integer.parseInt(st.nextToken()); // 바로 최종 배열에 추가
+        int list1_length = Integer.parseInt(st.nextToken());
+        int list2_length = Integer.parseInt(st.nextToken());
 
-            if(i == list1_length - 1){
-                st = new StringTokenizer(br.readLine()); // 두번ㅉ ㅐ배열에 저장할 정수 입력
+        int[] list1 = new int[list1_length];
+        int[] list2 = new int[list2_length];
+
+        st = new StringTokenizer(br.readLine()); // 첫번째 배열에 요소 추가
+        for (int i = 0; i < list1_length; i++) {
+            list1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine()); // 두번째 배열에 요소 추가
+        for (int i = 0; i < list2_length; i++) {
+            list2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        // 포인터로 활용될 변수 선언
+        int i = 0; 
+        int j = 0;
+
+        //정렬하면서 최종 배열에 요소 추가함
+        while (i < list1_length && j < list2_length) {
+            if (list1[i] <= list2[j]) {
+                sb.append(list1[i++]).append(" ");
+            }else{
+                sb.append(list2[j++]).append(" ");
             }
         }
 
-        sort(total_list);
-
-        for (int i : total_list) {
-            bw.write(i + " ");
+        // 요소 추가후 남은 요소들 추가
+        while (i < list1_length) {
+            sb.append(list1[i++]).append(" ");
         }
 
+        while (j < list2_length) {
+            sb.append(list2[j++]).append(" ");
+        }
+
+        // 출력
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
