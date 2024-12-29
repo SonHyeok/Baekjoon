7a5f900cef8e42a06ba6c4ac13f95de8581acda7
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-12-30 03:02:18 +0900

    [Silver III] Title: Four Squares, Time: 88 ms, Memory: 11860 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+/**
+ * 작은 문제들을 나열하다보면,  숫자 i는 자신의 제곱수(dp[j*j])들을 기준으로 제곱수를 뺀 나머지 값의 합을 구하면 된다.
+ * 따라서 아래와 같은 점화식을 세울 수 있다.
+ *  dp[i] = dp[i - j*j] + dp[j*j]
+ *
+ */
+public class Main {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력받을 직사각형 가로 길이
+        int N = Integer.parseInt(br.readLine());
+
+        // dp배열 초기화
+        dp = new int[N + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        solve(N);
+
+        bw.write(String.valueOf(dp[N]));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+    static void solve(int n) {
+        for (int i = 2; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+
+            dp[i] = min + 1;
+        }
+
+
+    }
+}
