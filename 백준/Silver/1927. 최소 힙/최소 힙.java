1cba545505eeb7045b7dcd9653aa4c9074160efb
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-01-13 23:02:54 +0900

    [Silver II] Title: 최소 힙, Time: 256 ms, Memory: 26932 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (queue.isEmpty() && num == 0) {
+                bw.write(0 + "\n");
+                continue;
+            } else if (num == 0) {
+                bw.write(queue.poll() + "\n");
+                continue;
+            }
+
+            queue.offer(num);
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+    }
+
+
+}
