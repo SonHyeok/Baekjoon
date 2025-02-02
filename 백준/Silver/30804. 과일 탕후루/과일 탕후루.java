458a6b05a4e4633998b436060efb65a4bf19766b
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-02-02 22:41:21 +0900

    [Silver II] Title: 과일 탕후루, Time: 336 ms, Memory: 38292 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,35 +1,56 @@
 import java.io.*;
 import java.util.*;
 
+
+/**
+ * 1~9까지의 번호가 붙어있는 과일 존재
+ * 앞,뒤로 과일들을 빼면서 과일의 종류를 체크
+ * 2가지의 과일로만 탕후루를 만들면서 과일의 갯수가 최대인 경우를 찾아야 함
+ * 투 포인터 알고리즘을 사용하여 풀이
+ *
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());  
-        int[] fruits = new int[N];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            fruits[i] = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        HashMap<Integer, Integer> fruitsCnt = new HashMap<>();
-        int maxLength = 0; 
-        int left = 0; 
+        int start = 0;
+        int result = 0;
 
-        for (int right = 0; right < N; right++) {
-            fruitsCnt.put(fruits[right], fruitsCnt.getOrDefault(fruits[right], 0) + 1);
+        for (int end = 0; end < count; end++) {
+            // map에 과일 추가
+            map.put(list.get(end), map.getOrDefault(list.get(end), 0) + 1);
 
-            while (fruitsCnt.size() > 2) {
-                fruitsCnt.put(fruits[left], fruitsCnt.get(fruits[left]) - 1);
+            // 과일 종류가 2개보다 많은 경우
+            while (map.size() > 2) {
+                // map에서 과일 차감
+                map.put(list.get(start), map.getOrDefault(list.get(start), 0) - 1);
 
-                if (fruitsCnt.get(fruits[left]) == 0) {
-                    fruitsCnt.remove(fruits[left]);
+                // 과일이 0개인 경우 제거
+                if (map.get(list.get(start)) == 0) {
+                    map.remove(list.get(start));
                 }
 
-                left++;
+                start++;
             }
-            maxLength = Math.max(maxLength, right - left + 1);
+            result = Math.max(result, end - start + 1);
         }
-        System.out.println(maxLength);
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
+
