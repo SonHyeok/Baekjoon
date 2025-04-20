f7dfa698d1aef9a0f36f02b746ed22090060334b
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-04-20 19:00:57 +0900

    [Gold IV] Title: N-Queen, Time: 2868 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,62 +1,68 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
- * 퀸은 가로,세로,대각선 방향으로 끝까지 이동 가능하다.
- * 행의 차와 열의 차가 같으면 대각선상에 위치하는 것이다.
+ * 퀸은 가로, 세로, 대각선으로 이동 가능
  *
- * 1차원 배열로 풀이가 가능한데, 그 이유는 1차원 배열의 인덱스를 열로 생각하고 해당 인덱스의 값을 행으로 생각하는것이다.
- * 예를 들면 [0,3,2,1]인 경우 0번째 행의 0번째 열, 1번째 행의 3번째 열, 2번째 행의 2번째 열, 3번째 행의 1번째 열로 생각하는 것이다.
- * 이후 퀸이 움직이는 가로, 세로 및 대각선 방향으로 검사를 하고, 퀸을 배치가 가능한 경우 배치한다.
+ *
+ *
+ *
  */
 public class Main {
-    static int[] list;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
     static int count = 0;
+    static int[] map; // 도시 정보
+    static boolean[] col;
+    static boolean[] diag1;
+    static boolean[] diag2;
 
     public static void main(String[] args) throws IOException {
 
         N = Integer.parseInt(br.readLine());
+        map = new int[N];
 
-        list = new int[N];
+        col = new boolean[N];
+        diag1 = new boolean[N * 2 - 1];
+        diag2 = new boolean[N * 2 - 1];
 
-        backTrack(0);
+        backTrack(0, 0);
+
         bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
         br.close();
+
     }
 
-    static void backTrack(int depth) {
-        // 퀸이 배치 가능한 위치에 모두 배치한 경우 1추가
+    static void backTrack(int row, int depth) {
+        // 모든 행에 퀸을 배치했을 때, 경우의 수 +1 추가
         if (depth == N) {
             count++;
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            list[depth] = i;
-            // 다음 행에 퀸 배치가 가능한 경우 백트래킹 재귀 호출
-            if (possibility(depth)) {
-                backTrack(depth + 1);
+        // 선택된 행의 모든 열 검사
+        for (int c = 0; c < N; c++) {
+            if (col[c] || diag1[row + c] || diag2[row - c + N - 1]) {
+                continue;
             }
-        }
-    }
 
-    static boolean possibility(int col) {
-        for (int i = 0; i < col; i++) {
-            // 해당 열의 행과 i열의 행이 일치하는 경우 (= 같은 행에 존재할 경우)
-            if (list[col] == list[i]) {
-                return false;
-            }
-            // 대각선상에 존재하는 경우
-            if (Math.abs(list[col] - list[i]) == Math.abs(col - i)) {
-                return false;
-            }
+            col[c] = true;
+            diag1[row + c] = true;
+            diag2[row - c + N - 1] = true;
+
+            backTrack(row + 1, depth + 1);
+
+            col[c] =false;
+            diag1[row + c] = false;
+            diag2[row - c + N - 1] = false;
 
         }
-        return true;
-    }
 
+    }
 }
+                
