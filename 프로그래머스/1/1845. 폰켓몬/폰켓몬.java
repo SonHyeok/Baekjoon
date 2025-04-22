95aca893eac90493d2c3538375f2906e35104d1b
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-04-22 14:22:22 +0900

    [level 1] Title: 폰켓몬, Time: 6.32 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,17 +3,16 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        int count = 0;
         
         Map<Integer,Integer> map = new HashMap<>();
         
         for(int n : nums) map.put(n, map.getOrDefault(n,0) + 1);
-        for(int key : map.keySet()) count += map.get(key);
-        
-        int half = count / 2; // 전체 포켓몬 수의 반
+
+        int half = nums.length / 2; // 전체 포켓몬 수의 반
         int size = map.keySet().size(); // 포켓몬 종류 수
+
         if(half >= size) answer = size;
-        else if(half <= size) answer = half;
+        else answer = half;
         
         
         return answer;
