c1a77f54e73d02f54a524b977a8918b5e31887ce
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-04-26 19:58:47 +0900

    베스트 앨범 클래스 사용한 풀이 추가

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -59,3 +59,67 @@ class Solution {
         return answer;
     }
 }
+
+// 아래는 클래스를 활용한 풀이
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public int[] solution(String[] genres, int[] plays) {
+        
+//         int[] answer = {};
+//         List<Integer> result = new ArrayList<>();
+        
+//         Map<String, List<Song>> songMap = new HashMap<>();
+//         Map<String, Integer> playMap = new HashMap<>();
+
+//         // 값 세팅
+//         for (int i = 0; i < genres.length; i++) {
+//             songMap.putIfAbsent(genres[i], new ArrayList<>());
+
+//             songMap.get(genres[i]).add(new Song(i, plays[i]));
+//             playMap.put(genres[i], playMap.getOrDefault(genres[i], 0) + plays[i]);
+//         }
+
+//         // 장르를 총 재생 수 기준으로 내림차순 정렬
+//         List<String> sortedGenres = new ArrayList<>(playMap.keySet());
+//         sortedGenres.sort((n1,n2) -> playMap.get(n2).compareTo(playMap.get(n1)));
+
+//         //각 장르별로 노래를 play 수, index 기준 정렬 후 2개씩 선택
+//         for (String key : sortedGenres) {
+//             List<Song> genre = songMap.get(key);
+//             genre.sort((n1,n2) -> {
+//                 if(n1.play != n2.play){
+//                     return n2.play - n1.play;
+//                 }else{
+//                     return n1.index - n2.index;
+//                 }
+//             });
+            
+//             // 최대 2곡 선택
+//             for (int i = 0; i < Math.min(2, genre.size()); i++) {
+//                 result.add(genre.get(i).index);
+//             }
+//         }
+
+//         answer = new int[result.size()];
+
+//         // List -> int[] 변환
+//         for (int i = 0; i < result.size(); i++) {
+//             answer[i] = result.get(i);
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+// class Song{
+//     int index;
+//     int play;
+
+//     Song(int index, int play){
+//         this.index = index;
+//         this.play = play;
+//     }
+// }
