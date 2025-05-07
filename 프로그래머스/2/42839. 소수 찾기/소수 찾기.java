d5b43b41378ee90bd3bd48e3daaad23853101cb6
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-05-07 12:39:40 +0900

    [level 2] Title: 소수 찾기, Time: 28.61 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    static Set<Integer> set = new HashSet<>();
+    static boolean[] visit = new boolean[7];
+    
+    public int solution(String numbers) {
+        int answer = 0;
+
+        dfs("",numbers,0);
+        
+        for(int num : set){
+            if(isPrime(num)) answer++;
+        }
+        return answer;
+    }
+    
+    static void dfs(String s, String numbers, int depth){
+        if(depth > numbers.length()) return;
+        
+        for(int i = 0; i < numbers.length(); i++){
+            if(!visit[i]){
+                visit[i] = true;
+                set.add(Integer.parseInt(s + numbers.charAt(i)));
+                dfs(s + numbers.charAt(i), numbers, depth + 1);
+                visit[i] = false;
+            }
+        }
+        
+    }
+    
+    static boolean isPrime(int num){
+        if(num < 2) return false;
+        
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
