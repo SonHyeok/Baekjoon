851124c63bed51a2dfef95261e0bd27969fbc31f
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-05-08 18:40:46 +0900

    [level 2] Title: 올바른 괄호, Time: 18.88 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        char[] charList = s.toCharArray();
+        
+        for(char c : charList){
+            if(c == '(') stack.push(c);
+            else if(!stack.isEmpty() && c == ')') stack.pop();
+            else {
+                answer = false;
+                break;
+            }
+        }
+        
+        if(!stack.isEmpty()) answer = false;
+
+        return answer;
+    }
+}
