b4f881bc2f26dd95221a05c53ebfd5c7f6855b53
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-05-13 20:32:31 +0900

    [level 2] Title: 소수 찾기, Time: 42.31 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,33 +1,44 @@
 import java.util.*;
 class Solution {
-    static Set<Integer> set = new HashSet<>();
-    static boolean[] visit = new boolean[7];
-    
-    public int solution(String numbers) {
-        int answer = 0;
 
-        dfs("",numbers);
+    static boolean[] isUsed;
+    public int solution(String numbers) {
         
-        for(int num : set){
-            if(num == 2) answer++;
-            if(num % 2 != 0 && isPrime(num)) answer++;
+        Set<Integer> set = new HashSet<>();
+        isUsed = new boolean[numbers.length()];
+        
+        find(numbers, "", set);
+        
+        int answer = 0;
+        
+        for(Integer s : set){
+            if(isPrime(s)) answer++;
         }
+        
+        
+        
         return answer;
     }
     
-    static void dfs(String s, String numbers){
-        int n = numbers.length();
-        
-        if(!s.equals("")) set.add(Integer.parseInt(s));
+    static void find(String numbers, String current, Set<Integer> set){
+        if(numbers.length() == current.length()) return;
         
         for(int i = 0; i < numbers.length(); i++){
-            dfs(s + numbers.charAt(i), numbers.substring(0,i) + numbers.substring(i + 1, n));
+            if(!isUsed[i]) {
+                isUsed[i] = true;
+                System.out.println(current + String.valueOf(numbers.charAt(i)));
+                set.add(Integer.parseInt(current + String.valueOf(numbers.charAt(i))));
+                find(numbers, current + String.valueOf(numbers.charAt(i)), set);
+                isUsed[i] = false;
+            }
         }
     }
     
     static boolean isPrime(int num){
         if(num < 2) return false;
-        
+        if(num == 2) return true;
+        if(num % 2 == 0) return false;
+
         for(int i = 3; i <= Math.sqrt(num); i += 2){
             if(num % i == 0) return false;
         }
