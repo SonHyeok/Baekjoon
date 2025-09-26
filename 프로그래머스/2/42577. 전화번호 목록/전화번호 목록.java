feab583106e4347e02ba9fc9bd076bcd9e199840
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-09-26 18:25:36 +0900

    [level 2] Title: 전화번호 목록, Time: 368.10 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,17 +1,16 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        
         Arrays.sort(phone_book);
         
-        // 다른 번호에서 접두사의 위치가 0번째 인덱스인 경우 false
         for(int i = 0; i < phone_book.length - 1; i++){
-            if(phone_book[i+1].indexOf(phone_book[i]) == 0){
-                answer = false;
-                break;
-            } 
+            String prefix = phone_book[i];            
+            String num = phone_book[i + 1];
+
+            if(num.startsWith(prefix) || prefix.startsWith(num)){
+                return false;
+            }
         }
-        return answer;
+        return true;
     }
 }
