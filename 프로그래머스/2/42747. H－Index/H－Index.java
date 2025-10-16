b183c9bba01b367cf9b2a2e6891fb6aeb9e01997
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-10-16 17:35:09 +0900

    [level 2] Title: H-Index, Time: 1.08 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,21 +4,18 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = Integer.MIN_VALUE;
-        int len = citations.length; // 논문 총 갯수       
-        
         Arrays.sort(citations);
+        int hIndex = 0;
         
-        for(int i = 0; i < len; i++){
-            // 일정 횟수 이상 인용된 논문의 갯수가 인용 횟수보다 크거나 같은 경우
-            if(citations[i] >= len - i){ // 여기서 len - i는 일정 횟수 이상 인용된 논문의 갯수를 의미
-                answer = len - i; // 해당 논문의 갯수를 최댓값으로 설정 후 종료
-                break;
-            }
+        for(int i = 0; i < citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length - i);
+            hIndex = Math.max(hIndex, smaller);
+            
         }
         
-        if(answer == Integer.MIN_VALUE) return 0;
-        
-        return answer;
+        return hIndex;
     }
 }
+
+
+// [2,2,3,3,5]
