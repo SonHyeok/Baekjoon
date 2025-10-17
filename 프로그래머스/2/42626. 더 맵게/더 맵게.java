cb509cc95e3b5682ae8f0d1a7bbc982a5c29c932
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-10-17 17:06:47 +0900

    [level 2] Title: 더 맵게, Time: 1463.39 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,27 +4,23 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
         
-        for(int n : scoville){
-            queue.offer(n);
-        } 
+        for(int sco : scoville){
+            heap.offer(sco);
+        }
         
-        //K보다 작은 숫자만큼 반복
-        while(queue.peek() < K && queue.size() > 1){
+        while(heap.peek() < K && heap.size() > 1){
+            int min1 = heap.poll();
+            int min2 = heap.poll();
             
-            int newNum = queue.poll() + (queue.poll() * 2);
-            queue.offer(newNum);
-            answer++; // 스코빌 섞는 횟수 증가
+            heap.offer(min1 + (min2 * 2));
+            answer++;
         }
         
-       int size = queue.size(); 
-        
-        // K이하인 지수가 있을 경우 -1 리턴
-        for(Iterator<Integer> it = queue.iterator(); it.hasNext();){
-            if(it.next() < K) return -1;
-        }
+        if(heap.peek() < K) return -1;
         
         return answer;
     }
+
 }
