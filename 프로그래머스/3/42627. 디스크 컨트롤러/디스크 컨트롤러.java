916de402d2094774a1594fa2ae7d88776db20518
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-10-21 20:31:44 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.94 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -11,19 +11,23 @@ class Solution {
         // 작업 시간 오름차순 정렬
         PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> a[1] - b[1]);
         
-        int currentTime = 0;
-        int idx = 0;
-        int count = 0;
-        int total = 0;
+        int currentTime = 0; // 현재 시간
+        int idx = 0; // 작업 인덱스
+        int count = 0; // 종료한 작업 갯수
+        int total = 0; // 총 반환 시간
         
+        // 종료한 작업 갯수가 jobs 길이과 같아질 때 까지 반복
         while(count < jobs.length){
+            // 이전 작업 진행중 진입한 작업 대기 큐에 추가
             while(idx < jobs.length && currentTime >= jobs[idx][0]){
                 queue.offer(jobs[idx++]);
             }
             
+            // 큐가 비어있는 경우 다음 작업 시작 시점으로 현재 시간 이동
             if(queue.isEmpty()){
                 currentTime = jobs[idx][0];
             }
+            // 큐가 비어있지 않는 경우
             else{
                 int[] job = queue.poll();
                 currentTime += job[1];
