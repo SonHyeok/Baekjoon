e19b910f704b52cef605f1928f0d37e635e11fbc
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2025-11-03 16:25:35 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.82 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,49 +1,45 @@
 import java.util.*;
 
+// 그래프 형태 생성
+// 하나씩 연결을 끊어보면서 끊은 이후 두 쪽으로 나눠진 전력망 중에서, 한 쪽의 갯수 측정
+// 이후 전체 갯수에서 측정된 갯수를 빼고 나머지 한 쪽의 갯수를 빼서 최솟값 측정
 class Solution {
     public int solution(int n, int[][] wires) {
+    
         int[][] graph = new int[n + 1][n + 1];
-        int minCount = 999;
+        int min = 999;
         
-        // 그래프 생성 및 초기화
+        // 그래프 생성
         for(int[] wire : wires){
             graph[wire[0]][wire[1]] = 1;
             graph[wire[1]][wire[0]] = 1;
         }
         
-        // 하나씩 연결 끊어가며 각 네트워크의 송전탑 갯수 측정
-        for(int[] wire : wires){
-            // 연결 끊기
+        for(int[] wire: wires){
+            boolean[] visited = new boolean[n + 1];
+                
             graph[wire[0]][wire[1]] = 0;
             graph[wire[1]][wire[0]] = 0;
-
-            boolean[] visited = new boolean[n + 1];
             
-            // 한쪽 송전탑 갯수 측정
-            int count = dfs(graph, visited, wire[0], n);
+            int cur = dfs(graph, visited, wire[0]);
             
-            // 한 쪽 송전탑의 갯수 - 다른 한 쪽 송전탑의 갯수 = 갯수 차이
-            // n - count가 측정하지 않은 다른 한 쪽의 송전탑 갯수
-            // count = 측정한 한 쪽의 송전탑 갯수
-            int diff = Math.abs((n - count) - count);
+            int diff = Math.abs((n - cur) - cur);
+            min = Math.min(min, diff);
             
-            // 연결 복구
             graph[wire[0]][wire[1]] = 1;
             graph[wire[1]][wire[0]] = 1;
-            
-            minCount = Math.min(minCount, diff);
-        }
+        } 
         
-        return minCount;
+        return min;
     }
     
-    int dfs(int[][] graph, boolean[] visited, int node, int n){
+    int dfs(int[][] graph, boolean[] visited, int node){
         visited[node] = true;
         int count = 1;
         
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i < graph.length; i++){
             if(graph[node][i] == 1 && !visited[i]){
-                count += dfs(graph, visited, i, n);
+                count += dfs(graph, visited, i);
             }
         }
         
